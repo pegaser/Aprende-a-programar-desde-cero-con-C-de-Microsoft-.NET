a936faa85fc325f00fca7f3595c23cc83d94cb35
Sergio Alejandro Pérez Gabriel <sperezgabriel@gmail.com>
2020-08-13 14:37:36 -0500

    Actualizacion de ejemplos del curso

---
+++ UsingAsyncAwait/UsingAsyncAwaitExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingAsyncAwaitExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sites = new List<string> { "http://www.microsoft.com", "http://www.google.com", "http://www.msn.com" };
+            List<Task<string>> tasks = new List<Task<string>>();
+            foreach (string site in sites)
+            {
+                tasks.Add(DownloadContent(site));
+            }
+            int index = Task.WaitAny(tasks.ToArray());
+            Task<string> taskCompleted = tasks.ToArray()[index];
+            Console.WriteLine($"El sitio que respondió primero fue: {taskCompleted.Result} ");
+            Console.Read();
+
+        }
+        public static async Task<string> DownloadContent(string site)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync(site);
+                return site;
+            }
+        }
+    }
+}
--- UsingAsyncAwait/UsingConfigureAwait/MainWindow.xaml.cs
+++ UsingAsyncAwait/UsingConfigureAwait/MainWindow.xaml.cs
@@ -17,13 +17,10 @@ namespace UsingConfigureAwait
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            //HttpClient httpClient = new HttpClient();
-            //string content = await httpClient.GetStringAsync("http://www.microsoft.com").ConfigureAwait(false);
-            //Output.Content = content;
             HttpClient httpClient = new HttpClient();
             string content = await httpClient.GetStringAsync("http://www.microsoft.com").ConfigureAwait(false);
-            
-            using (FileStream sourceStream = new FileStream("temp.html", FileMode.Create, FileAccess.Write, FileShare.None,4096, useAsync: true))
+
+            using (FileStream sourceStream = new FileStream("temp.html", FileMode.Create, FileAccess.Write, FileShare.None, 4096, useAsync: true))
             {
                 byte[] encodedText = Encoding.Unicode.GetBytes(content);
                 await sourceStream.WriteAsync(encodedText, 0, encodedText.Length).ConfigureAwait(false);
