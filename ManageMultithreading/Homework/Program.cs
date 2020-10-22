5cf1553c971cb7bf3ef82a4aadaabdb850fe3098
Sergio Alejandro Pérez Gabriel <sperezgabriel@gmail.com>
2020-10-22 16:02:11 -0500

    Actualizando tarea de Administrando multithreading

---
+++ ManageMultithreading/Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+            int counter = 0;
+            
+            Console.WriteLine("Presiona una tecla para iniciar el contador y vuelve a presionar otra tecla para cancelarlo.");
+            Console.ReadLine();
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    counter++;
+                    Console.Write($"{counter} ");
+                    Thread.Sleep(500);
+                }
+            }, token);
+
+            Console.ReadLine();
+            cancellationTokenSource.Cancel();
+        }
+
+    }
+}
