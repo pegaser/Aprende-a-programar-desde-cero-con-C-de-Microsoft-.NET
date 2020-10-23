6b4a85d77446729a0878f99142726f8f16a8ec4c
Sergio Alejandro Pérez Gabriel <sperezgabriel@gmail.com>
2020-10-23 10:06:42 -0500

    Actualización de tarea en sección Tipos en modo Avanzado

---
+++ TypesAdvanced/Homework/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework
+{
+    class Program
+    {
+        static List<T> GetInitializedList<T>(T value, int count)
+        {
+            List<T> list = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(value);
+            }
+            return list;
+        }
+
+        static void Main()
+        {
+            List<bool> list1 = GetInitializedList(true, 7);
+            List<string> list2 = GetInitializedList<string>("C#", 3);
+            foreach (bool value in list1)
+            {
+                Console.WriteLine(value);
+            }
+            foreach (string value in list2)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
