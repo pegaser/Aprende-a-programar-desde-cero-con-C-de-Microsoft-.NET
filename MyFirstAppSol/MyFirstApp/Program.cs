3025ed26983b061ca694b4c96df48bed7d3eec08
Sergio Alejandro Pérez Gabriel <sperezgabriel@gmail.com>
2020-11-24 14:52:08 -0600

    Cambios para MyFirstApp con .NET

---
+++ MyFirstAppSol/MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My First Hello World!");
+        }
+    }
+}
---
+++ MyFirstAppSol/MySecondApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySecondApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Second Hello World!");
+        }
+    }
+}
