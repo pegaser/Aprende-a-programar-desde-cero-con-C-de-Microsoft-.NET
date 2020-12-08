b404fa5f2e7e177432e8c193ba98246f0717ddef
Sergio Alejandro Pérez Gabriel <sperezgabriel@gmail.com>
2020-12-07 21:13:54 -0600

    Actualización de StudentScores

---
+++ StudentScores/StudentScores/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentNames = new string[] { "Sergio", "Alejadro", "Gabriel" };
+            SortedList<string, int> sortedStudentScores = new SortedList<string, int>();
+            int score;
+            foreach (var item in studentNames)
+            {
+                score = 0;
+                while (score == 0)
+                {
+                    Console.WriteLine($"Proporciona la calificación de {item}");
+                    GetInteger(Console.ReadLine(), out score);
+                }
+                sortedStudentScores.Add(item, score);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (var item in sortedStudentScores)
+            {
+                Console.WriteLine($"Estudiante: {item.Key} Calificación: {item.Value}");
+            }
+            Console.ReadKey();
+        }
+
+        private static void GetInteger(string data, out int score)
+        {
+            if (!int.TryParse(data, out score))
+            {
+                Console.WriteLine("El dato debe ser entero");
+            }
+        }
+    }
+}
