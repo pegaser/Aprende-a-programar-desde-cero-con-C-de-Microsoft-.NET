c4474831a49166306c35038d46106f1067cf9b22
Sergio Alejandro Pérez Gabriel <sperezgabriel@gmail.com>
2020-12-11 11:35:05 -0600

    Actualización de EventsAndCallbacks

---
+++ EventsAndCallbacks/.vs/ManuallyRisingEventExceptionHandling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManuallyRisingEventExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
