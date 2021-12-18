d46ab07561a8e1fdd79928a13701228587c3e724
pegaser <sperezgabriel@gmail.com>
2021-12-17 18:14:56 -0600

    Add RecordTypeExample December 2021

---
+++ RecordTypeExampleSol/RecordTypeExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RecordTypeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet myDog = new Pet("Firulais", 3);
+            Pet yourDog = myDog with { };
+            Console.WriteLine($"¿Mi perro es tu perro? {myDog == yourDog}");
+            Console.WriteLine(myDog);
+            Console.ReadKey();
+        }
+    }
+    public record struct Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Pet(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
