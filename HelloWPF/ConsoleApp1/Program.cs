cf79534a72cd4bba7c0e3ff37c6f583eeb6ad3d4
pegaser <sperezgabriel@gmail.com>
2022-01-26 11:22:45 -0600

    Actualización Enero 2022

---
+++ HelloWPF/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+List<int> list = new List<int>();
+list.Add(1);
+list.Add(2);
+foreach(int i in list)
+{
+    Console.WriteLine(i);
+}
---
+++ HelloWPF/DataBindingExample/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Person person = new Person() 
+            {
+                Name="Sergio",
+                Age=43
+             };
+
+            this.DataContext = person;
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
