12a3ff4e3590eb386b07d262c4272f95237c5787
pegaser <sperezgabriel@gmail.com>
2023-12-01 19:16:30 -0600

    Upgrade to C#12

---
+++ PrimaryConstructorsExampleSol/PrimaryConstructorsExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PrimaryConstructorsExample
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dog myDog = new Dog("Canelo", "Pérez");
+			//myDog.DogName = "Canelo";
+			//myDog.FamilyName = "Pérez";
+			Console.WriteLine(myDog.FullName);
+			myDog.AddApostrophe();
+			Console.WriteLine(myDog.FullName);
+
+			Dog mySecondDog = new Dog("Bonie");
+			Console.WriteLine(mySecondDog.FullName);
+
+			Console.ReadKey();
+		}
+	}
+	public class Dog(string DogName, string FamilyName)
+	{
+		//public string FullName
+		//{
+		//	get
+		//	{
+		//		return $"{DogName} {FamilyName}";
+		//	}
+		//}
+		public string FullName => $"{DogName} {FamilyName}";
+		public void AddApostrophe()
+		{
+			DogName = $"'{DogName}'";
+		}
+
+		public Dog(string dogName) : this(dogName, "Sin Apellido")
+		{
+
+		}
+	}
+}
