6c80f6906231e0fac65c5c72854e0e06fe0c623b
pegaser <sperezgabriel@gmail.com>
2023-12-05 14:25:42 -0600

    Upgrade to DotNet8

---
+++ CollectionExpressionExample/CollectionExpressionExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionExpressionExample
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> weekDays1 = new List<string>() { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" } ;
+			List<string> weekDays2 = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+			string[] weekDays3 = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+			foreach (var day in weekDays3) 
+			{
+				Console.WriteLine(day);
+			}
+
+			string[] vowels = ["a", "e", "i", "o", "u"];
+			string[] consonants = ["b",
+				"c",
+				"d",
+				"f",
+				"g",
+				"h",
+				"j",
+				"k",
+				"l",
+				"m",
+				"n",
+				"p",
+				"q",
+				"r",
+				"s",
+				"t",
+				"v",
+				"w",
+				"x",
+				"z"];
+			string[] alphabet = [..vowels, ..consonants, "y"];
+			foreach (var letter in alphabet)
+			{
+				Console.WriteLine(letter);
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ InputParametersLambdaExpressions/InputParametersLambdaExpressions/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace InputParametersLambdaExpressions
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Action line = () => Console.WriteLine();
+			Console.WriteLine("Hello");
+			line();
+			Console.WriteLine("World");
+			Console.ReadKey();
+
+			Func<double,double> cube = x => x * x * x;
+			line();
+			Console.WriteLine(cube(3));
+			Console.ReadKey();
+
+			Func<int, int, bool> testForEquality = (x, y) => x == y;
+			line();
+			Console.WriteLine(testForEquality(1,2));
+			Console.ReadKey();
+
+			var isTooLong = (int x, string s) => s.Length > x;
+			line();
+			Console.WriteLine(isTooLong(3, "ABCDE"));
+			Console.ReadKey();
+
+			var IncrementBy = (int source, int increment = 1) => source + increment;
+			line();
+			Console.WriteLine(IncrementBy(5));
+			Console.WriteLine(IncrementBy(5, 2));
+			Console.ReadKey();
+
+		}
+	}
+}
