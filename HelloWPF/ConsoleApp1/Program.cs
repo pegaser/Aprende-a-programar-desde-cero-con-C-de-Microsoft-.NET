c0612bb90274ca49be2b5f16a26c289d275187d0
pegaser <sperezgabriel@gmail.com>
2025-02-21 16:25:39 -0600

    Update to DotNet9

--- HelloWPF/ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Hello, World!");
-List<int> list = new List<int>();
-list.Add(1);
-list.Add(2);
-foreach(int i in list)
-{
-    Console.WriteLine(i);
-}
--- MyFirstWpfApp/MyFirstWpfApp/App.xaml.cs
+++ MyFirstWpfApp/MyFirstWpfApp/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace MyFirstWpfApp
@@ -14,4 +10,5 @@ namespace MyFirstWpfApp
     public partial class App : Application
     {
     }
+
 }
